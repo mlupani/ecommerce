a31d35b2c2c21aa027d46ffbdd39b4c0b510606d
Miguel Lupani <mlupani2@gmail.com>
2024-11-17 12:11:11 -0300

    cambio

--- server/database/config.js
+++ server/database/config.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-
         await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
